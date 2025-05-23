32c6234eaa9cf85b9f94dfa37e667db372f44848
Ana <awwribeiro@gmail.com>
2025-05-23 11:49:33 -0300

    component perfil

--- src/app/pages/perfil-form/perfil-form.component.ts
+++ src/app/pages/perfil-form/perfil-form.component.ts
@@ -1,11 +1,14 @@
+import { Habilidade } from './../../shared/models/habilidade.interface';
 
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { ButtonComponent } from '../../shared/components/button/button.component';
-import { Habilidade } from '../../shared/models/habilidade.interface';
-import { ChipComponent } from '../../shared/components/chip/chip.component';
+import { Router } from '@angular/router';
+import { CadastroService } from '../../shared/services/cadastro.service';
+import { ChipComponent } from "../../shared/components/chip/chip.component";
+import { Idioma } from '../../shared/models/idioma.interface';
 
 
 @Component({
@@ -14,14 +17,15 @@ import { ChipComponent } from '../../shared/components/chip/chip.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    ButtonComponent
-  ],
+    ButtonComponent,
+    ChipComponent
+],
   templateUrl: './perfil-form.component.html',
   styleUrls: ['./perfil-form.component.scss']
 })
-export class PerfilFormComponent {
+export class PerfilFormComponent implements OnInit {
   perfilForm!: FormGroup;
-  fotoPreview: string | ArrayBuffer | undefined;
+  fotoPreview!: string | ArrayBuffer | null;
 
   habilidades: Habilidade[] = [
     { nome: 'Fullstack', selecionada: false },
@@ -41,10 +45,107 @@ export class PerfilFormComponent {
   idiomas: string[] = [
     'Português',
     'Inglês',
-    'Espanhol'
+    'Espanhol',
+    'Françes',
+    'Italiano',
+    'Alemão',
+    'Japones',
+    'Chines'
   ];
 
-  onAnterior(): void {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private cadastroService: CadastroService
+  ) {}
 
-  onProximo(): void {}
+  ngOnInit(): void {
+    this.inicializarFormulario();
+  }
+
+  onAnterior(): void {
+    this.salvarDadosAtuais();
+    this.router.navigate(['/cadastro/dados-pessoais']);
+  }
+
+  onProximo(): void {
+    if(this.perfilForm.valid){
+      this.salvarDadosAtuais();
+      this.router.navigate(['/cadastro/confirmacao']);
+    }
+  }
+
+  onFotoSelecionada(event: any): void { //representa o evento de alteração (change) de um campo de arquivo (<input type="file">)
+    const file = event.target.files[0]; //Acessa o primeiro arquivo selecionado pelo usuário no input de arquivos
+    if(file) {
+      const reader = new FileReader(); //FileReader API que permite aplicativos da web leiam de forma assíncrona o conteúdo de arquivos armazenados no computador do usuário
+      reader.onload = () => { //será executada automaticamente quando a leitura do arquivo for concluída com sucesso.
+        this.fotoPreview = reader.result; //Armazena o resultado da leitura do arquivo
+        this.perfilForm.patchValue( {foto: reader.result}); //patchValue é usado para atualizar parcialmente o formulário, sem precisar preencher todos os campos.
+      }
+      reader.readAsDataURL(file); // transforma a imagem em uma string que pode ser usada diretamente em um atributo src de imagem
+    }
+  }
+
+  toggleHabilidade(habilidade: Habilidade): void {
+    habilidade.selecionada = !habilidade.selecionada; //alterna o estado
+    //filter pega apenas as habilidades que estão selecionadas
+    //map transforma essa lista em um array contendo apenas os nomes dessas habilidades
+    const habilidadesSelecionadas = this.habilidades.filter(h => h.selecionada).map(h => h.nome);
+    this.perfilForm.patchValue({ habilidadesSelecionadas }); //patchValua atualiza somente o campo habilidades
+  }
+
+  get idiomasArray(): FormArray {
+    return this.perfilForm.get('idiomas') as FormArray; // acessa o array de idiomas do formulário
+  }
+
+  adicionarIdioma(nome: string = '', nivel: string = ''): void {
+    const idiomaForm = this.fb.group({ //Cria um novo FormGroup chamado idiomaForm, que representa um idioma
+      nome: [nome, Validators.required],
+      nivel: [nivel, Validators.required]
+    });
+    this.idiomasArray.push(idiomaForm); //acessando novo FormGroup
+  }
+
+  removerIdioma(index: number): void {
+    if(index === 0 && this.idiomasArray.at(0).get('nome')?.value === 'Português') { //impede a remoção do "Português" no índice 0
+      return;
+    }
+  }
+
+  inicializarFormulario(): void {
+    this.perfilForm = this.fb.group({
+      foto: [''],
+      resumo: [''],
+      habilidadesSelecionadas: [[]],
+      idiomas: this.fb.array([]),
+      portifolio: [''],
+      linkedin: ['']
+    });
+
+    this.adicionarIdioma('Português', 'Nativo');
+  }
+
+  private extrairIdiomas(): Idioma[] {
+    return this.idiomasArray.controls.map(control => {
+      return {
+        nome: control.get('nome')?.value,
+        nivel: control.get('nivel')?.value
+      };
+    });
+  }
+
+  private salvarDadosAtuais() {
+    const formValue = this.perfilForm.value;
+
+    this.cadastroService.updateCadastroData({
+      foto: this.fotoPreview,
+      resumo: formValue.resumo,
+      habilidadesSelecionadas: formValue.habilidadesSelecionadas,
+      idiomas: this.extrairIdiomas(),
+      portifolio: formValue.portifolio,
+      linkedin: formValue.linkedin
+    })
+  }
+
 }
